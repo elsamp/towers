e8aebef442aaf261028ac4557a6f813e37ad8edc
Erica <ericabcherry@gmail.com>
2015-10-06 20:29:13 -0400

    Time Scale script for beanier to speed up time :)

---
+++ Assets/Scripts/Utilities/TimeScaleManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeScaleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyUp(KeyCode.Keypad1))
+        {
+            Time.timeScale = 1;
+        }else if(Input.GetKeyUp(KeyCode.Keypad2))
+        {
+            Time.timeScale = 2;
+        }
+        else if (Input.GetKeyUp(KeyCode.Keypad3))
+        {
+            Time.timeScale = 3;
+        }
+
+    }
+}

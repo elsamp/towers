0122dc15c130e315bf2922c2b2aca3437fd7ca28
Erica <ericabcherry@gmail.com>
2015-10-12 15:00:34 -0400

    adjusted elemental chance - reduced to 10%

--- Assets/Scripts/Levels/GemSelector.cs
+++ Assets/Scripts/Levels/GemSelector.cs
@@ -59,17 +59,17 @@ public class GemSelector : MonoBehaviour {
 
             if (GUI.Button(new Rect(10, 10, 50, 20), "cold"))
             {
-                ink = new Ink(0.3f,0,0,0.5f,Ink.InkElement.Cold);
+                ink = new Ink(0.3f,0,0,0.1f,Ink.InkElement.Cold);
             }
 
             if (GUI.Button(new Rect(70, 10, 50, 20), "earth"))
             {
-                ink = new Ink(0, 0, 0.3f,0.5f, Ink.InkElement.Earth);
+                ink = new Ink(0, 0, 0.3f,0.1f, Ink.InkElement.Earth);
             }
 
             if (GUI.Button(new Rect(140, 10, 50, 20), "fire"))
             {
-                ink = new Ink(0, 0.3f, 0, 0.5f, Ink.InkElement.Fire);
+                ink = new Ink(0, 0.3f, 0, 0.1f, Ink.InkElement.Fire);
             }
         }
 	}

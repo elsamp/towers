b75678772a20e7c0a5a35948b08c989b07ffc1ab
Erica <ericabcherry@gmail.com>
2015-10-18 20:17:14 -0400

    Starting to get the GUI working. rough UI elements in place and empty scripts attached.

---
+++ Assets/Scripts/UI/BattleUI/RuneList/RuneListItem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RuneListItem : MonoBehaviour {
+
+    public Image runeImage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setRuneImage(string resourceName)
+    {
+        this.GetComponentInChildren<Text>().text = resourceName;
+    }
+}
---
+++ Assets/Scripts/UI/BattleUI/RuneList/RuneListPanel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuneListPanel : MonoBehaviour {
+
+    public RuneItemContainer itemContainer;
+    public Gem[] availibleRunes;
+
+    // Use this for initialization
+    void Start () {
+        populateContainerWithRunes(availibleRunes);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void populateContainerWithRunes(Gem[] gemsToPopulate)
+    {
+        itemContainer.populateWithRunes(gemsToPopulate);
+    }
+}
